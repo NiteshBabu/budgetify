f93cf61c4c0e60c4361159fadd2d93b355452848
NiteshBabu <niteshbabusharma@gmail.com>
2026-01-16 12:54:35 +0530

    chore: update packages & switch to bun build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,10 @@
 import RootProvider from '@/components/providers/RootProvider'
 import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import { Toaster } from 'sonner'
 import './globals.css'
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-})
 
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
 	title: 'Budgetify',
@@ -34,7 +25,7 @@ export default function RootLayout({
 					colorScheme: 'dark',
 				}}>
 				<body
-					className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+					className={`font-mono antialiased`}>
 					<Toaster richColors position='bottom-right' />
 					<RootProvider>{children}</RootProvider>
 				</body>
